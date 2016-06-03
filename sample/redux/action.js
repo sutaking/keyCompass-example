55b88117e8895037e1990eb98c6dda258b6b1e9d
sutaking <zhaofeng.vip@gmail.com>
2016-06-03 14:33:06 +0800

    update

--- sample/redux/action.js
+++ sample/redux/action.js
@@ -9,6 +9,9 @@ const TALKS_ORDER = '&order=created_at:desc';
 const TALKS_LIMIT = '&limit=20';
 const PHOTO_URL = '&photo_url_sizes=615x461,800x600,1600x1200,original';
 
+const GET_TALK_LISTS = 'GET_TALK_LISTS';
+const GET_TALK = 'GET_TALK';
+
 export function testJsonp() {
     //var url = 'https://app-api.ted.com/v1/talks/ids.json?sort=popular&podcasts=true&api-key=b26yxyqwkx8fmtuttfavssf6';
     var url = REQUEST_HEADER +REQUEST_TALKS+ API_KEY + TALKS_FIELDS + TALKS_SORT + TALKS_ORDER
@@ -21,9 +24,13 @@ export function testJsonp() {
       }).catch(function(ex) {
         console.log('parsing failed', ex)
       })
-
 };
 
-function fetchTalkLists () {
+export function updataTalkLists (index) {
+    return {
+        type: GET_TALK_LISTS,
+        index
+    }
+};
 
-}
+
